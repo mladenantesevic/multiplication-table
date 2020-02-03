ab3097f9093ccb5c0b8f10f9f7bc62ec66509452
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-03 21:14:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+
+include 'config.php';
+try
+{
+    $host=$config['DB_HOST'];
+    $dbname=$config['DB_DATABASE'];
+$conn= new PDO("mysql:host=$host;dbname=$dbname",$config['DB_USERNAME'],$config['DB_PASSWORD']);
+//new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+}
+catch(PDOException $e)
+{
+    echo "Error:".$e->getMessage();
+}
+
 for ($i = 1; $i < 10; $i++) {
   for ($j = 1; $j < 10; $j++) {
      if ($j == 1) {
