9d126ede55044e863d685dfc7e68be73dd8801ab
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-04 00:09:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,3 +25,24 @@ echo "<table border =\"1\" style='border-collapse: collapse'>";
 		echo "</table>";
 ?>
 ?>
+
+    <?php
+      
+        if(isset($_POST['button1'])) { 
+            echo "This is Button1 that is selected"; 
+        } 
+        if(isset($_POST['button2'])) { 
+            echo "This is Button2 that is selected"; 
+        } 
+    ?> 
+      
+    <form method="post"> 
+        <input type="submit" name="button1"
+                value="Button1"/> 
+          
+        <input type="submit" name="button2"
+                value="Button2"/> 
+    </form> 
+</head> 
+  
+</html> 
