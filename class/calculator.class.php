7337ad7df32a934fc9a5df38cc3dac3cc7df1dbf
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-04 10:23:08 +0100

    Create calculator.class.php

---
+++ class/calculator.class.php
@@ -0,0 +1,40 @@
+<?php 
+
+class calculator
+{
+    var $a;
+    var $b;
+
+    function checkoperation($oprator)
+    {
+        switch($oprator)
+        {
+            case '+':
+            return $this->a + $this->b;
+            break;
+
+            case '-':
+            return $this->a - $this->b;
+            break;
+
+            case '*':
+            return $this->a * $this->b;
+            break;
+
+            case '/':
+            return $this->a / $this->b;
+            break;
+
+            default:
+            return "Sorry No command found";
+        }   
+    }
+    function getresult($a, $b, $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        return $this->checkoperation($c);
+    }
+}
+
+?>
