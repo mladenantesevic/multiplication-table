034352297eb9ace6979a2333e8946b55aefc4be2
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-04 16:48:05 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
+<script>
+highlight_row();
+function highlight_row() {
+    var table = document.getElementById('display-table');
 var cells = table.getElementsByTagName("td"); 
 for (var i = 0; i < cells.length; i++) { 
    cells[i].onclick = function(){tes();};
 }
+</script>
