dc82e4029cef81563f1e9f159a69f4de67a9d1c6
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-04 13:56:17 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,4 +5,17 @@ $config=array(
 'DB_PASSWORD'=>'',
 'DB_DATABASE'=>'gobinath'
 );
+
+try
+{
+    $host=$config['DB_HOST'];
+    $dbname=$config['DB_DATABASE'];
+$conn= new PDO("mysql:host=$host;dbname=$dbname",$config['DB_USERNAME'],$config['DB_PASSWORD']);
+//new PDO("mysql:host=$hostname;dbname=mysql", $username, $password);
+}
+catch(PDOException $e)
+{
+    echo "Error:".$e->getMessage();
+}
+
 ?>
