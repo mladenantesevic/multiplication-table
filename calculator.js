a07a5096c22e50a8fe446020f9383657242e5790
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-04 23:42:57 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,10 +1,15 @@
+//javascript koji na klik salje ajax request sa ID-om polja na kojem je bio klik
+
 <script>
 show_result();
 function show_result() {
 var table = document.getElementById('display-table');
 var cells = table.getElementsByTagName("td"); 
 for (var i = 0; i < cells.length; i++) { 
-   cells[i].onclick = function(){tes();};
-    }
+   cells[i].onclick = function(){
+   xhttp.open("GET", "calculator.php?cell=$i", true);
+   xhttp.send();
+   };
+   }
 }
 </script>
