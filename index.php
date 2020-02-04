abfcaf6b19bc05e6caa6c628cd0c729fd5343331
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-04 12:24:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -18,18 +18,79 @@ catch(PDOException $e)
     echo "Error:".$e->getMessage();
 }
 
-echo "<table border =\"1\" style='border-collapse: collapse'>";
-	for ($row=1; $row <= 10; $row++) { 
-		echo "<tr> \n";
-		for ($col=1; $col <= 10; $col++) { 
-		   $p = $col * $row;
-		   echo "<td>$row . " x " . $col . " = "$p</td> \n";
-		   	}
-	  	    echo "</tr>";
-		}
-		echo "</table>";
-?>
-?>
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<link rel="stylesheet" type="text/css" href="layout.css">";
+echo "</head>";
+<body>
+<table id="display-table" class="table-layout">
+    <thead>
+        <th>ID</th>
+        <th>Company</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>100</td>
+            <td>Abc</td>
+        </tr>
+        <tr>
+            <td>101</td>
+            <td>Def</td>
+        </tr>
+        <tr>
+            <td>102</td>
+            <td>Ghi</td>
+        </tr>
+    </tbody>
+</table>
+
+<script>
+highlight_row();
+function highlight_row() {
+    var table = document.getElementById('display-table');
+    var cells = table.getElementsByTagName('td');
+
+    for (var i = 0; i < cells.length; i++) {
+        // Take each cell
+        var cell = cells[i];
+        // do something on onclick event for cell
+        cell.onclick = function () {
+            // Get the row id where the cell exists
+            var rowId = this.parentNode.rowIndex;
+
+            var rowsNotSelected = table.getElementsByTagName('tr');
+            for (var row = 0; row < rowsNotSelected.length; row++) {
+                rowsNotSelected[row].style.backgroundColor = "";
+                rowsNotSelected[row].classList.remove('selected');
+            }
+            var rowSelected = table.getElementsByTagName('tr')[rowId];
+            rowSelected.style.backgroundColor = "yellow";
+            rowSelected.className += " selected";
+
+            msg = 'The ID of the company is: ' + rowSelected.cells[0].innerHTML;
+            msg += '\nThe cell value is: ' + this.innerHTML;
+            alert(msg);
+        }
+    }
+
+}
+</script>
+</body>
+</html>
+
+//echo "<table border =\"1\" style='border-collapse: collapse'>";
+//	for ($row=1; $row <= 10; $row++) { 
+//		echo "<tr> \n";
+//		for ($col=1; $col <= 10; $col++) { 
+//		   $p = $col * $row;
+//		   echo "<td>$row . " x " . $col . " = "$p</td> \n";
+//		   	}
+//	  	    echo "</tr>";
+//		}
+//		echo "</table>";
+//?>
+//?>
 
 $cal = new calculator();
 if(isset($_POST['submit']))
