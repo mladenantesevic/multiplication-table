8a667d1a7c1c5e3e1e34e7e6e72c4ebdbe9eab13
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-05 23:12:14 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,6 +4,4 @@ $username = "username";
 $password = "password";
 $dbname = "myDB";
 
-//potrebno je u bazi kreirati tabelu Multiplications sa sledecim poljima: (multiplicatorA, multiplicatorB, product, date)
-
 ?>
