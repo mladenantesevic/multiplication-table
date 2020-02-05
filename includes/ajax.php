22e5d28cd9e1a38af0ab008e2c39981b1fc0d3df
mladenantesevic <31984273+mladenantesevic@users.noreply.github.com>
2020-02-05 10:50:49 +0100

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -1,15 +1,14 @@
 <?php
 
+require_once(__DIR__.'/class/calculator.class.php');
+require_once(__DIR__.'/includes/config.php');
+
 cell = $_GET['cell'];
 
-// a = cell/10 +1
-// b = cell/10 ostatak dijeljenja
+$p = new Calculator($cell);
 
-
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+//$a = (int) ($cell / 10) + 1;
+//$b = $cell % 10;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -18,8 +17,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+$sql = "INSERT INTO Multiplications (multiplicatorA, multiplicatorB, product, date)
+VALUES ($a, $b, $p, date("Y-m-d H:i:s"))";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
